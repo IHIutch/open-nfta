70c0672f10874aab5503a1b95e61bfaa056c39f3
IHIutch <jbhutch01@gmail.com>
2021-12-20 08:46:03 -0500

    Install qs

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,12 @@ import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { ChakraProvider } from "@chakra-ui/react";
 import { useState } from "react";
+import qs from "qs";
+import axios from "redaxios";
+
+axios.defaults.paramsSerializer = (params) => {
+  return qs.stringify(params);
+};
 
 function MyApp({ Component, pageProps }) {
   const [queryClient] = useState(
